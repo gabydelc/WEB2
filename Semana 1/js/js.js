e34a37c4a33d41e2aca3160679d4aa7ccdfddb82
gabydelc <gabrieladelcastillo_95@hotmail.com>
2016-02-19 23:00:16 -0400

    desktop

---
+++ Semana 1/js/js.js
@@ -0,0 +1,10 @@
+
+var playBtn = document.getElementById('play');
+var stopBtn = document.getElementById('stop');
+
+var playSound = function() {
+  audio.play();
+};
+
+playBtn.addEventListener('click', playSound, false);
+stopBtn.addEventListener('click', function(){audio.pause()}, false);
