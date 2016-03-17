7524046ee70949d5a512a636af45b7bcb0bba470
gabydelc <gabrieladelcastillo_95@hotmail.com>
2016-03-17 12:40:27 -0400

    s8

--- Semana 6 /js.js
+++ Semana 6 /js.js
@@ -6,6 +6,40 @@ var playSound = function() {
   audio.play();
 };
 
-playBtn.addEventListener('click', playSound, false);
-stopBtn.addEventListener('click', function(){audio.pause()}, false);
+//segundo sonido - play
 
+playBtn.addEventListener('click', playSound, false);
+stopBtn.addEventListener('click', function(){song.pause()}, false);
+
+
+
+  $(document).ready(function () {
+          var songElement = document.createElement('song');
+          audioElement.setAttribute('src', 'wakenbake.mp3');
+          audioElement.setAttribute('autoplay:false', 'autoplay');
+          //audioElement.load code above. if you take out :false from the code the file will auto play than everythin works the same after that()
+          $.get();
+          songElement.addEventListener("load", function () {
+              songElement.play();
+          }, true);
+
+
+          $(document).keypress(function (e) {
+              if (e.which == 13) { //press enter the audio will play
+                  songElement.play();
+
+              } else if (e.which == 32) { //press spacebar the audio will                             pause play
+                  songElement.pause();
+              }
+          });
+
+// the code below wil allow you to click the play and stop button with the mouse
+          $('.play-button').click(function () { 
+              songElement.play();
+          });
+
+
+          $('.pause').click(function () {
+              songElement.pause();
+          });
+      });
